772fad86e78f99abb2e8db328b4ce39982690d18
Edu Andrade <eduardo.a@edu.pucrs.br>
2022-11-28 16:06:09 -0300

    Camera working

--- Python-Tracking/main.py
+++ Python-Tracking/main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import socket
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+success, img = cap.read()
+h, w, _ = img.shape
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while True:
+    # Get image frame
+    success, img = cap.read()
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)  # with draw
+    # hands = detector.findHands(img, draw=False)  # without draw
+    data = []
 
+    if hands:
+        # Hand 1
+        hand = hands[0]
+        lmList = hand["lmList"]  # List of 21 Landmark points
+        for lm in lmList:
+            data.extend([lm[0], h - lm[1], lm[2]])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        sock.sendto(str.encode(str(data)), serverAddressPort)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
